456a3b654302ce1a970d2d9b78f47d682721a773
gerard-mrtnz <133831424+gerard-mrtnz@users.noreply.github.com>
2023-05-16 19:53:23 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,233 @@
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    <style>
+        body {
+            background-color: #D10000 !important;
+            color: white;
+        }
+    </style>
+
+    <title>Site principal</title>
+  </head>
+
+  <body>
+
+    <div class="container-fluid">
+      <!-- Barra de navegación -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">INFORMÁTICA</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/productos.php">Productos</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="pages/aboutus.php">Requisitos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/contactenos.php">Contáctanos</a>
+            </li>Requisito
+          </ul>
+        </div>
+      </nav>
+
+      <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img/images.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/youtube-icon.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+
+    <br><br>
+    <h1 class="titulo">Tema de investigación seleccionado: YouTube</h1>
+
+    <div class="container conte">
+      <div class="row">
+        <div class="col-md-4 h-100">
+          <div class="card d-flex">
+            <img src="img/youtube.jpg" class="card-img-top" alt="Foto 1">
+            <div class="card-body">
+              <p class="card-text">Un programa mundialmente famoso</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 h-100">
+          <div class="card d-flex">
+            <img src="img/youtube-red-logo.jpg" class="card-img-top" alt="Foto 2">
+            <div class="card-body">
+              <p class="card-text">Una de las mejores aplicaciones para ver videos en muchas plataformas</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 h-100">
+          <div class="card d-flex">
+            <img src="img/you.tube_.logo_.4.jpg" class="card-img-top" alt="Foto 3">
+            <div class="card-body">
+              <p class="card-text">Gran herramienta para cualquier persona</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <section id="mision-vision-valores" class="bg-dark py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12" style="justify-content: center; text-align: center;">
+              <h1>DEFINICIÓN:</h1>
+            </div>
+          </div>
+          <div class="row m-2">
+            <div class="col-md-12">
+              YouTube es una plataforma de alojamiento de videos en línea que utiliza la tecnología
+              de streaming para permitir a los usuarios cargar, ver y compartir videos en Internet.
+              Utiliza una arquitectura distribuida para almacenar y entregar los videos, con múltiples
+              servidores que alojan los archivos de video y los distribuyen a través de una red de
+              entrega de contenido (CDN) para asegurar una carga rápida y una reproducción fluida.
+              YouTube utiliza algoritmos de compresión de video y técnicas de transcodificación para
+              convertir los archivos de video en formatos que puedan ser transmitidos en diferentes
+              dispositivos y conexiones a Internet. Además, cuenta con herramientas de administración
+              de contenido para que los creadores de contenido puedan administrar sus canales,
+              interactuar con su audiencia y generar ingresos a través de la publicidad y otras
+              opciones de monetización.
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12" style="justify-content: center; text-align: center;">
+              <h3>CARACTERISTICAS</h3>
+            </div>
+          </div>
+          <div class="row m-2">
+            <div class="col-md-12">
+              Alojamiento de video en línea: YouTube permite a los usuarios cargar y almacenar videos en línea para que puedan ser compartidos y vistos por otros usuarios en todo el mundo.
+              <br><br>
+              Streaming de video: Los videos en YouTube se pueden ver en línea sin tener que descargarlos completamente, gracias a la tecnología de streaming que permite una visualización fluida.
+              <br><br>
+              Red social: YouTube es una red social en sí misma, con opciones para que los usuarios se suscriban a canales, sigan a otros usuarios y se comuniquen a través de comentarios.
+              <br><br>
+              Amplia variedad de contenido: Los usuarios pueden encontrar videos sobre prácticamente cualquier tema en YouTube, desde música y entretenimiento hasta noticias, educación y tutoriales.
+              <br><br>
+              Monetización: Los creadores de contenido pueden monetizar sus videos a través de publicidad y otras opciones, lo que les permite generar ingresos a partir de su contenido.
+              <br><br>
+              Comunidad global: YouTube cuenta con una amplia comunidad global de usuarios y creadores de contenido, lo que lo convierte en un lugar para conectar con personas de todo el mundo.
+              <br><br>
+              Herramientas de edición: YouTube cuenta con herramientas de edición de video en línea que permiten a los usuarios editar sus videos directamente en la plataforma.
+              <br><br>
+              Accesibilidad: YouTube ofrece opciones de accesibilidad, como subtítulos y descripciones de audio, para garantizar que todos los usuarios puedan disfrutar del contenido.
+              <br><br>
+              Analytics: Los creadores de contenido tienen acceso a herramientas analíticas que les permiten medir el rendimiento de sus videos y comprender mejor a su audiencia.
+              <br><br>
+              Personalización: YouTube ofrece opciones de personalización para que los usuarios puedan ajustar su experiencia de visualización y descubrimiento de contenido en función de sus intereses.
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12 mt-5" style="justify-content: center; text-align: center;">
+        <h5>Qué es YouTube?</h5>
+      </div>
+      <div class="col-md-12" style="justify-content: center; text-align: center;">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/uu-5kZbx9hM" title="Qué es YouTube" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12 mt-5" style="justify-content: center; text-align: center;">
+        <h5>Descargue mi archivo presionando el siguiente botón!</h5>
+      </div>
+      <div class="col-md-12 mb-4" style="justify-content: center; text-align: center;">
+        <button class="btn btn-primary" id="download-btn">Para bajar documento completo presione clic aquí</button>
+      </div>
+    </div>
+
+    <footer class="bg-dark text-center text-white">
+      <!-- Grid container -->
+      <div class="container p-4 pb-0">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+          <!-- Facebook -->
+          <a href="https://www.facebook.com/" target="_blank" class="iconos-footer"><i class="bi bi-facebook display-6" target="_blank"></i></a>
+
+          <!-- Twitter -->
+          <a href="https://twitter.com/" target="_blank" class="iconos-footer"><i class="bi bi-twitter display-6"></i></a>
+
+          <!-- Instagram -->
+          <a href="https://www.instagram.com" class="iconos-footer" target="_blank"><i class="bi bi-instagram display-6"></i></a>
+
+          <!-- tiktok -->
+          <a href="https://www.tiktok.com" class="iconos-footer" target="_blank"><i class="bi bi-tiktok display-6" target="_blank"></i></a>
+
+          <!-- YouTube -->
+          <a href="https://www.youtube.com" class="iconos-footer" target="_blank"><i class="bi bi-youtube display-6" target="_blank"></i></a>
+        </section>
+        <!-- Section: Social media -->
+      </div>
+      <!-- Grid container -->
+
+      <!-- Copyright -->
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2023 Copyright:
+        Gerardo Faustino Jovel Martinez
+      </div>
+      <!-- Copyright -->
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js" integrity="sha256-a2yjHM4jnF9f54xUQakjZGaqYs/V1CYvWpoqZzC2/Bw=" crossorigin="anonymous"></script>
+
+  </body>
+
+  <!-- Incluye SweetAlert2 -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+  <!-- Script para manejar la descarga del archivo y mostrar la alerta -->
+  <script>
+    // Obtener el ID del archivo de Google Drive
+    const downloadBtn = document.getElementById('download-btn');
+    downloadBtn.addEventListener('click', () => {
+      const url = 'https://drive.google.com/u/0/uc?id=1-8ByM8YzOXLz6FF9qrn9yu1J6KmviDtV&export=download';
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'tarea.pdf';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      Swal.fire('¡Gracias!', 'Por descargar la tarea de Gerardo', 'success');
+    });
+  </script>
+
+  </html>
